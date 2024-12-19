a7914e4e9d5f0cf36ac0e317583f042fce30c491
ahmad-hamdani <ahmadhamdaniiii2020@gmail.com>
2024-12-19 17:42:16 +0700

    penambahan sempel bottom nav

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      dark_blue: '#000412',
+      dark_blue_smooth: '#001022'
+    },
+    fontFamily: {
+      'oswald': ['Oswald'],
+    },
+  },
+  plugins: [],
+}
+
